9d4b2bc729a29f10cede4900dd687cd69f93a86f
rebeccapatek <rebeccapatek@gmail.com>
2021-03-13 11:29:08 -0600

    phase 8 reducing redundancy in code

--- Program.cs
+++ Program.cs
@@ -16,62 +16,25 @@ namespace EnthusiasticMoose
 
        // Ask a question
 
-    CanadaQuestion();
-    EnthusiasticQuestion();
-    LoveCSharpQuestion();
-    SecretQuestion();
+    Question("Is Canada real?", "Really? It seems very unlikely.", "I  K N E W  I T !!!" );
+    Question("Are you enthusiastic?", "Yay!", "You should try it!");
+    Question("Do you love C# yet?", "Good job sucking up to your instructor!", "You will...oh, yes, you will..." );
+    Question("Do you want to know a secret?", "ME TOO!!!! I love secrets...tell me one!", "Oh, no...secrets are the best, I love to share them!" );
         }
 
-    static void CanadaQuestion()
+    static void Question(string Ask, string Yes, string No)
 {
-    bool isTrue = MooseAsks("Is Canada real?");
+    bool isTrue = MooseAsks(Ask);
     if (isTrue)
     {
-        MooseSays("Really? It seems very unlikely.");
+        MooseSays(Yes);
     }
     else
     {
-        MooseSays("I  K N E W  I T !!!");
-    }
-}
-    static void EnthusiasticQuestion()
-{
-    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-    if (isEnthusiastic)
-    {
-        MooseSays("Yay!");
-    }
-    else
-    {
-        MooseSays("You should try it!");
+        MooseSays(No);
     }
 }
 
-static void LoveCSharpQuestion()
-{
-    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-    if (doesLoveCSharp)
-    {
-        MooseSays("Good job sucking up to your instructor!");
-    }
-    else
-    {
-        MooseSays("You will...oh, yes, you will...");
-    }
-}
-
-static void SecretQuestion()
-{
-    bool isEverythingFine = MooseAsks("Do you want to know a secret?");
-    if (isEverythingFine)
-    {
-        MooseSays("ME TOO!!!! I love secrets...tell me one!");
-    }
-    else
-    {
-        MooseSays("Oh, no...secrets are the best, I love to share them!");
-    }
-}
 
     static bool MooseAsks(string question)
 {
