99d6e348fc36628f89c1781979e10dbbc526850b
rebeccapatek <rebeccapatek@gmail.com>
2021-03-13 10:11:52 -0600

    the moose speaks

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace EnthusiasticMoose
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+            MooseSays();
         }
+
+        static void MooseSays()
+        {
+            Console.WriteLine(@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
+        }
+        
     }
+    
 }
+
