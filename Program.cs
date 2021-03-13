9d06b73d015c0b0ae576928b8c0b30c3f175423e
rebeccapatek <rebeccapatek@gmail.com>
2021-03-13 10:40:21 -0600

    phase 7

--- Program.cs
+++ Program.cs
@@ -15,6 +15,15 @@ namespace EnthusiasticMoose
     MooseSays("I really am enthusiastic");
 
        // Ask a question
+
+    CanadaQuestion();
+    EnthusiasticQuestion();
+    LoveCSharpQuestion();
+    SecretQuestion();
+        }
+
+    static void CanadaQuestion()
+{
     bool isTrue = MooseAsks("Is Canada real?");
     if (isTrue)
     {
@@ -24,7 +33,46 @@ namespace EnthusiasticMoose
     {
         MooseSays("I  K N E W  I T !!!");
     }
-        }
+}
+    static void EnthusiasticQuestion()
+{
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+}
+
+static void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+static void SecretQuestion()
+{
+    bool isEverythingFine = MooseAsks("Do you want to know a secret?");
+    if (isEverythingFine)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
+}
+
     static bool MooseAsks(string question)
 {
     Console.Write($"{question} (Y/N): ");
